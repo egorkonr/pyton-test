25eb14b4d08a956b01d9407421ddd969b8bbf352
egorrjn <Spawn607@rambler.ru>
2023-08-21 11:05:09 +0500

    add

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 
-print("Hello worjd")
+slovo = str(input())
+x = len(slovo)
+i = 0
+x = x - 1
+k = 0
+while x - i >= i:
+    if slovo[x - i] == slovo[i]:
+        i += 1
+    else:
+        k = 1
+        break
+if k == 1:
+  print("no")
+else:
+  print("yes")
